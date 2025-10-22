26b8b02e6313f9339567852925b6b2f81344fed3
xkhxl <87169182+xkhxl@users.noreply.github.com>
2025-10-22 20:51:09 +0530

    Add index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello from nodejs-demo-app!'));
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
